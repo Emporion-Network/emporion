4c0a6e804c73d3f26bdb7c68a3bd8753f1b11159
Cesium133 <oussama@example.com>
2025-02-28 23:14:14 -0500

    Publishing patch version updated backend to 0.0.9

--- backend/src/services/metadata/index.ts
+++ backend/src/services/metadata/index.ts
@@ -18,7 +18,7 @@ const app = new Hono<{ Variables: { state: State } }>()
 
     const ids = await Promise.all(metadata.map(async (m: ProductMetadata) => {
       const id = randomUUIDv7();
-      const url = `${state.domainName}/api/metadata/${id}`;
+      const url = `https://${state.domainName}/api/metadata/${id}`;
       await state.fs.write(`metadata/${id}`, JSON.stringify(m), {
         type: 'application/json',
         acl: 'public-read',
