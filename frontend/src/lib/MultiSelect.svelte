4bf9529a14b011e40117344fe37313e20d062e4c
Cesium133 <oussama@example.com>
2025-03-13 22:41:31 -0400

    Publishing patch version updated frontend to 0.0.53

--- frontend/src/lib/MultiSelect.svelte
+++ frontend/src/lib/MultiSelect.svelte
@@ -276,6 +276,7 @@
         outline: none;
         display: flex;
         gap: 0.5rem;
+        padding: 0.1rem 0;
         justify-content: start;
         align-items: center;
         cursor: pointer;
